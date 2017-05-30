8b4924b873dfb99393b9bf397b4fcd090a6c5f56
Aidan Edwards <Aidan.S.Edwards@gmail.com>
2017-05-29 22:53:24 -0500

    Websocket stuff almost done. After that, will need  a way to use it.

--- discordpp/Websocket.hh
+++ discordpp/Websocket.hh
@@ -5,10 +5,30 @@
 #ifndef DISCORDPP_WEBSOCKET_HH
 #define DISCORDPP_WEBSOCKET_HH
 
+#include <beast/websocket.hpp>
 
-class Websocket{
+#include <boost/asio.hpp>
+#include <boost/asio/ssl.hpp>
 
-};
+#include <json.hpp>
 
+namespace discordpp{
+	using json = nlohmann::json;
+
+	typedef std::function<void(beast::error_code const&, std::shared_ptr<beast::multi_buffer>, std::shared_ptr<beast::websocket::opcode>)> ws_callback_handler;
+
+	class Websocket{
+	public:
+		Websocket(boost::asio::io_service *ios, std::string token, std::string gateway, ws_callback_handler handler);
+
+		~Websocket();
+
+	private:
+		std::string const host_;
+		boost::asio::io_service *ios_;
+		std::string token_;
+		std::shared_ptr<beast::websocket::stream<boost::asio::ssl::stream<boost::asio::ip::tcp::socket> > > ws_;
+	};
+}
 
 #endif //DISCORDPP_WEBSOCKET_HH
--- src/Websocket.cc
+++ src/Websocket.cc
@@ -3,3 +3,63 @@
 //
 
 #include "discordpp/Websocket.hh"
+
+using namespace discordpp;
+
+Websocket::Websocket(boost::asio::io_service *ios, std::string token, std::string gateway, ws_callback_handler handler)
+		: host_("discordapp.com")
+		, ios_(ios)
+		, token_(token){
+	std::string::size_type protocolPos = gateway.find("wss://");
+	if (protocolPos != std::string::npos) {
+		gateway.erase(protocolPos, 6);
+	}
+	std::cout << "Connecting to gateway at " << gateway << "\n";
+	boost::asio::ip::tcp::resolver r{*ios_};
+	boost::asio::ip::tcp::socket sock{*ios_};
+	boost::asio::connect(sock, r.resolve(boost::asio::ip::tcp::resolver::query{gateway, "https"}));
+
+	// Perform SSL handshaking
+	boost::asio::ssl::context ctx{boost::asio::ssl::context::sslv23};
+	boost::asio::ssl::stream<boost::asio::ip::tcp::socket&> stream{sock, ctx};
+	stream.set_verify_mode(boost::asio::ssl::verify_none);
+	stream.handshake(boost::asio::ssl::stream_base::client);
+
+	// Secure WebSocket connect and send message using Beast
+	ws_ = std::make_shared<beast::websocket::stream<boost::asio::ssl::stream<boost::asio::ip::tcp::socket> >>(stream);
+	//ws_ = &ws;
+	std::cout << "handshaking at " << gateway << "\n";
+	ws_->handshake(gateway, "/");
+
+
+	std::cout << "Connection established.\n";
+
+	json connect = {
+			{"op", 2},
+			{"d", {
+					       {"token", token_},
+					       {"v", 4},
+					       {"properties", {
+							                      {"$os", "linux"},
+							                      {"$browser", "discordpp"},
+							                      {"$device", "discordpp"},
+							                      {"$referrer",""}, {"$referring_domain",""}
+					                      }
+					       },
+					       {"compress", false},
+					       {"large_threshold", 250}
+			       }
+			}
+	};
+	std::cout << "Client Handshake:\n" << connect.dump(1) << "\n";
+
+	ws_->write(boost::asio::buffer(connect.dump()));
+
+	std::shared_ptr<beast::multi_buffer> b;
+	std::shared_ptr<beast::websocket::opcode> op;
+	ws_->async_read(*op, *b, std::bind(handler, this, std::placeholders::_1, op, b));
+}
+
+Websocket::~Websocket(){
+	//ws_->close(beast::websocket::close_code::normal);
+}
